8082140fe086e53e35af7ca99c3dc489b7a55f81
husanibragimov <ibragmovhusan283@gmail.com>
2024-04-28 15:23:24 +0500

    Fix typo in IsTecherPermission class in permissions.py

--- apps/common/permissions.py
+++ apps/common/permissions.py
@@ -10,6 +10,6 @@ class IsAdminPermission(permissions.BasePermission):
 
 class IsTecherPermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.status == 'teacher' or request.user.status == 'admin':
+        if request.user.status == 'teacher' or 'admin':
             return True
         return False
